82720836fe06d7429485906c7cb6cc073a46cd06
Alban Rahier <ara.rahier@gmail.com>
2019-06-13 11:53:22 +0200

    :construction: Add a database connected to api_ddale

--- app/main.py
+++ app/main.py
@@ -1,9 +1,35 @@
 from fastapi import FastAPI, HTTPException
 from starlette.status import HTTP_404_NOT_FOUND
-from models import Oeuvre,Calque,TypeCalque
+from models import Oeuvre,Calque,TypeCalque,OeuvreDb,CalqueDb
 from datetime import datetime
 from starlette.staticfiles import StaticFiles
+from starlette.responses import Response
+from starlette.requests import Request
+from sqlalchemy.orm import validates
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import Session, sessionmaker
 
+SQLALCHEMY_DATABASE_URI = "sqlite:///./database_ddale.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+Base.metadata.create_all(bind=engine)
+db_session = SessionLocal()
+
+db_session.close()
+
+def get_db(request: Request):
+    return request.state.db
+
+def get_oeuvre(db_session: Session, oeuvre_id: int) -> OeuvreDb:
+    return db_session.query(OeuvreDb).filter(OeuvreDb.id == oeuvre_id).first()
 
 app = FastAPI()
 
@@ -14,18 +40,27 @@ oeuvres = [
     ])
 ]
 
-
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/oeuvres/{oeuvre_id}", response_model=Oeuvre)
-async def read_oeuvre(oeuvre_id: int):
+async def read_oeuvre(oeuvre_id: int, db: Session = Depends(get_db)):
     try:
-        oeuvre = next((x for x in oeuvres if x.id == oeuvre_id))
-    except StopIteration:
+        oeuvre = get_oeuvre(db, oeuvre_id=oeuvre_id)
+    except FileNotFoundError:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND)
     return oeuvre
 
 app.mount("/calques", StaticFiles(directory="calques"), name="calque")
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next) -> Response:
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from pydantic import BaseModel,UrlStr,Schema, PositiveInt
 from datetime import datetime
 from typing import List
 from enum import Enum
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
+
 
 class TypeCalque(Enum):
     composition = "composition"
@@ -11,7 +15,7 @@ class Calque(BaseModel):
     id:int = Schema (..., gt=0,description="Id de l'oeuvre")
     typeCalque: TypeCalque = Schema (..., description="Type de calque (composition, anecdote ...")
     description:str =Schema (...,min_length=1, description="Description du calque")
-    urlCalque:UrlStr = Schema (..., description="Url de l'image du calque")
+    urlCalque: UrlStr = Schema (..., description="Url de l'image du calque")
 
 class Oeuvre(BaseModel):
     id:int = Schema(..., gt=0, description="Id de l'oeuvre")
@@ -24,4 +28,25 @@ class Oeuvre(BaseModel):
     urlCible:UrlStr = Schema (...,min_length=1, description="Url de l'image du tableau")
     calques:List[Calque] = Schema (..., description="Calques contenant des informations sur l'oeuvre")
 
+class OeuvreDb(Base):
+    __tablename__ = "oeuvres"
+    id = Column(Integer, primary_key=True, nullable=False)
+    titre = Column(Integer, primary_key=True, nullable=False)
+    auteur = Column(String, nullable=False)
+    technique = Column(String, nullable=False)
+    hauteur = Column(Integer, nullable = False)
+    largeur = Column(Integer, nullable = False)
+    annee = Column(Integer, nullable = False)
+    urlCible = Column(String, nullable=False)
+    calques_id = Column(String, ForeignKey("calques.id"), nullable=False)
+    calques = relationship("Calque", back_populates="oeuvre")
+
+class CalqueDb(Base):
+    __tablename__ = "calques"
+    id = Column(Integer, primary_key=True, nullable=False)
+    typeCalque = Column(String,nullable=False)
+    description = Column(String, nullable=False)
+    urlCalque = Column(String, nullable=False)
+    oeuvres_id = Column(String, ForeignKey("oeuvres.id"), nullable=False)
+    oeuvres = relationship("Oeuvre", back_populates="calque") #mettre un DB après Oeuvre ?
 
